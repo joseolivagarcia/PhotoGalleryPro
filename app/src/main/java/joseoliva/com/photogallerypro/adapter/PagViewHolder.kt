b7f537f817c7d698d54a537f088c381522662615
Jose Oliva Local <jose@joseoliva.com>
2022-10-04 10:22:14 +0200

    Funcionando la seleccion de imagenes y su guardado en bbdd. Problema con Provider

--- app/src/main/java/joseoliva/com/photogallerypro/adapter/PagViewHolder.kt
+++ app/src/main/java/joseoliva/com/photogallerypro/adapter/PagViewHolder.kt
@@ -1,6 +1,8 @@
 package joseoliva.com.photogallerypro.adapter
 
+import android.net.Uri
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import joseoliva.com.photogallerypro.R
@@ -9,9 +11,11 @@ import joseoliva.com.photogallerypro.bbdd.Imagenes
 class PagViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     val textoPrueba = view.findViewById<TextView>(R.id.tvtextopruebas)
+    val imagen = view.findViewById<ImageView>(R.id.ivfotografia)
 
     fun render(paginaImagen: Imagenes){
-        textoPrueba.setText(paginaImagen.uri)
+        textoPrueba.setText(paginaImagen.descripcion)
+        imagen.setImageURI(Uri.parse(paginaImagen.uri))
     }
 
 }
